da645db55a006b85771c27c7f0426e456e3d842b
Bormotov <bormotovk@gmail.com>
2022-02-02 14:42:52 +0300

    .

--- src/app/modules/dashboard/components/widget-header/widget-header.component.ts
+++ src/app/modules/dashboard/components/widget-header/widget-header.component.ts
@@ -36,7 +36,7 @@ export class WidgetHeaderComponent implements OnInit, OnDestroy {
       filter((s) : s is AnySettings => !!s),
       map(s => {
         this.settings = s;
-        const prefix = s.title?.split(' ')[0];
+        const prefix = s.title?.split(' ')[0] ?? '';
         if (isInstrumentDependent(s)) {
           const group = s.instrumentGroup;
           s.title = `${prefix} ${s.symbol} (${group ? group : ''})`
