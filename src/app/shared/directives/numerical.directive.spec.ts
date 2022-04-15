a81fce685430914b50e27ac46d74660e4163e997
Bormotov <bormotovk@gmail.com>
2022-04-15 11:42:00 +0300

    Fixed stop orders bugs and removed unnessesary backend estimate calls

---
+++ src/app/shared/directives/numerical.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { NumericalDirective } from './numerical.directive';
+
+describe('Directive: Numerical', () => {
+  it('should create an instance', () => {
+    const directive = new NumericalDirective();
+    expect(directive).toBeTruthy();
+  });
+});
