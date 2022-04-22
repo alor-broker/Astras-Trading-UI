ad9621e2c366120d90625410f5d561d0bb14c32b
sbelashevskiy <sbelashevskiy@gmail.com>
2022-04-22 09:45:49 +0500

    Issue #72: Fixed saving to local storage on add/remove instruments

--- src/app/shared/utils/testing.ts
+++ src/app/shared/utils/testing.ts
@@ -1,7 +1,39 @@
-import { defer } from "rxjs";
+import { defer } from 'rxjs';
+import { Instrument } from '../models/instruments/instrument.model';
 
 /** Create async observable that emits-once and completes
  *  after a JS engine turn */
 export function asyncData<T>(data: T) {
   return defer(() => Promise.resolve(data));
+}
+
+export class TestData {
+  public static get instruments(): Instrument[] {
+    return [
+      {
+        symbol: 'AAPL',
+        exchange: 'SPBX',
+        instrumentGroup: 'SPBX',
+        isin: 'US0378331005'
+      },
+      {
+        symbol: 'DSKY',
+        exchange: 'MOEX',
+        instrumentGroup: 'TQBR',
+        isin: 'RU000A0JSQ90',
+      },
+      {
+        symbol: 'SBER',
+        exchange: 'MOEX',
+        instrumentGroup: 'TQBR',
+        isin: 'RU0009029540',
+      },
+      {
+        symbol: 'DVEC',
+        exchange: 'MOEX',
+        instrumentGroup: 'TQBR',
+        isin: 'RU000A0JP2W1',
+      }
+    ];
+  }
 }
