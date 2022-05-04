96bbef826e0ab8798bfe7ad9dd750ee11db27e3d
sbelashevskiy <sbelashevskiy@gmail.com>
2022-05-04 18:07:52 +0500

    Issue #95: Added column for position value

--- src/app/shared/models/settings/blotter-settings.model.ts
+++ src/app/shared/models/settings/blotter-settings.model.ts
@@ -45,7 +45,8 @@ export const allPositionsColumns: ColumnIds[] = [
   { columnId: 'qtyT0', name: "T0", isDefault: false  },
   { columnId: 'qtyT1', name: "T1", isDefault: false  },
   { columnId: 'qtyT2', name: "T2", isDefault: true  },
-  { columnId: 'qtyTFuture', name: "TFuture", isDefault: false  }
+  { columnId: 'qtyTFuture', name: "TFuture", isDefault: false  },
+  { columnId: 'avgValue', name: "Стоимость", isDefault: false  },
 ];
 
 export const allTradesColumns: ColumnIds[] = [
