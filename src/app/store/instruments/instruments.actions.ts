5035bb542aeb2617c530efb41574da9437c47d52
Bormotov <bormotovk@gmail.com>
2022-05-16 14:34:54 +0300

    Rewrited loadInstruments

--- src/app/store/instruments/instruments.actions.ts
+++ src/app/store/instruments/instruments.actions.ts
@@ -2,8 +2,5 @@ import { createAction, props } from '@ngrx/store';
 import { InstrumentKey } from 'src/app/shared/models/instruments/instrument-key.model';
 import { Instrument } from '../../shared/models/instruments/instrument.model';
 
-export const selectNewInstrumentName = '[Instruments] SelectNewInstrument';
-export const newInstrumentSelectedName = '[Instruments] NewInstrumentSelected';
-
-export const selectNewInstrument = createAction(selectNewInstrumentName, props<{ instrument: InstrumentKey }>());
-export const newInstrumentSelected = createAction(newInstrumentSelectedName, props<{ instrument: Instrument }>());
+export const selectNewInstrument = createAction('[Instruments] SelectNewInstrument', props<{ instrument: InstrumentKey }>());
+export const newInstrumentSelected = createAction('[Instruments] NewInstrumentSelected', props<{ instrument: Instrument }>());
--- src/app/store/instruments/instruments.effects.ts
+++ src/app/store/instruments/instruments.effects.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { InstrumentsService } from 'src/app/modules/instruments/services/instruments.service';
-import { mergeMap, map, tap } from 'rxjs/operators';
+import { mergeMap, map, filter } from 'rxjs/operators';
 import { Instrument } from 'src/app/shared/models/instruments/instrument.model';
-import { selectNewInstrument, newInstrumentSelectedName } from './instruments.actions';
+import {
+  selectNewInstrument,
+  newInstrumentSelected,
+} from './instruments.actions';
 import { ErrorHandlerService } from 'src/app/shared/services/handle-error/error-handler.service';
 import { catchHttpError } from 'src/app/shared/utils/observable-helper';
 
@@ -15,21 +18,13 @@ export class InstrumentsEffects {
     private errorHandlerService: ErrorHandlerService
   ) {}
 
-  private instrument: Instrument | null = null;
-
   loadInstruments$ = createEffect(() =>
     this.actions$.pipe(
       ofType(selectNewInstrument),
       mergeMap((action) => this.instrumentsService.getInstrument(action.instrument)),
-      tap(instrument => this.instrument = instrument),
-      map((instrument) => ({
-        type: newInstrumentSelectedName,
-        instrument: instrument,
-      })),
-      catchHttpError({
-        type: newInstrumentSelectedName,
-        instrument: this.instrument
-      }, this.errorHandlerService)
+      catchHttpError<Instrument | null>(null, this.errorHandlerService),
+      filter((instrument): instrument is Instrument => !!instrument),
+      map((instrument) => newInstrumentSelected({ instrument }))
     )
   );
 }
