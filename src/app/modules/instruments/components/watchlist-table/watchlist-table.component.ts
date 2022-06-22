73680ee30352e935c854380530ad2a685e0ebe6e
Daniil Markov <d.markov@intellect.group>
2022-06-22 14:47:22 +0300

    add columns settings

--- src/app/modules/instruments/components/watchlist-table/watchlist-table.component.ts
+++ src/app/modules/instruments/components/watchlist-table/watchlist-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable, of, switchMap, take } from 'rxjs';
+import { Observable, of, switchMap, take, tap } from 'rxjs';
 import { WatchedInstrument } from '../../models/watched-instrument.model';
 import { WatchInstrumentsService } from '../../services/watch-instruments.service';
 import { selectNewInstrument } from '../../../../store/instruments/instruments.actions';
@@ -8,6 +8,7 @@ import { InstrumentKey } from '../../../../shared/models/instruments/instrument-
 import { WatchlistCollectionService } from '../../services/watchlist-collection.service';
 import { filter, map } from 'rxjs/operators';
 import { getPropertyFromPath } from "../../../../shared/utils/object-helper";
+import { allInstrumentsColumns, ColumnIds } from "../../../../shared/models/settings/instrument-select-settings.model";
 
 @Component({
   selector: 'ats-watchlist-table[guid]',
@@ -19,8 +20,9 @@ export class WatchlistTableComponent implements OnInit {
   guid!: string;
 
   watchedInstruments$: Observable<WatchedInstrument[]> = of([]);
+  displayedColumns: ColumnIds[] = [];
 
-  sortFns = {
+  sortFns: { [keyName: string]: (a: InstrumentKey, b: InstrumentKey) => number } = {
     symbol: this.getSortFn('instrument.symbol'),
     price: this.getSortFn('price'),
     dayChange: this.getSortFn('dayChange'),
@@ -41,6 +43,9 @@ export class WatchlistTableComponent implements OnInit {
 
   ngOnInit(): void {
     this.watchedInstruments$ = this.watchInstrumentsService.getSettings(this.guid).pipe(
+      tap(settings => {
+        this.displayedColumns = allInstrumentsColumns.filter(c => settings.instrumentColumns.includes(c.columnId));
+      }),
       switchMap(settings => this.watchInstrumentsService.getWatched(settings)),
     );
   }
@@ -61,6 +66,10 @@ export class WatchlistTableComponent implements OnInit {
 
   getTrackKey(index: number, item: WatchedInstrument): string {
     return WatchlistCollectionService.getInstrumentKey(item.instrument);
+  }
+
+  isVisibleColumn(colName: string): boolean {
+    return this.displayedColumns.map(c => c.columnId).includes(colName);
   }
 
   private getSortFn(propName: string): (a: InstrumentKey, b: InstrumentKey) => number {
