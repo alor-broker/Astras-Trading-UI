cbc4fcc1da3612e8bcb67114185b0bf046307e04
sbelashevskiy <sbelashevskiy@gmail.com>
2022-06-23 11:44:58 +0500

    [Common]: Fix eslint and tests

--- src/app/modules/command/components/limit-command/limit-command.component.spec.ts
+++ src/app/modules/command/components/limit-command/limit-command.component.spec.ts
@@ -2,12 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommandsService } from '../../services/commands.service';
 
 import { LimitCommandComponent } from './limit-command.component';
+import { of } from "rxjs";
 
 describe('LimitCommandComponent', () => {
   let component: LimitCommandComponent;
   let fixture: ComponentFixture<LimitCommandComponent>;
 
-  const spyCommands = jasmine.createSpyObj('CommandsService', ['setLimitCommand']);
+  const spyCommands = jasmine.createSpyObj('CommandsService', ['setLimitCommand', 'priceSelected$']);
+  spyCommands.priceSelected$ = of(1);
+
 
   beforeAll(() => TestBed.resetTestingModule());
   beforeEach(async () => {
