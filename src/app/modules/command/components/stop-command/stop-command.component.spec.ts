62319740a8542a675a9eb540d23932ff39d83b43
sbelashevskiy <sbelashevskiy@gmail.com>
2022-08-09 15:55:12 +0500

    Issue #181: Try to fix test

--- src/app/modules/command/components/stop-command/stop-command.component.spec.ts
+++ src/app/modules/command/components/stop-command/stop-command.component.spec.ts
@@ -26,6 +26,10 @@ import {
   getUtcNow
 } from "../../../../shared/utils/datetime";
 import { StopCommand } from "../../models/stop-command.model";
+import {
+  NZ_I18N,
+  ru_RU
+} from "ng-zorro-antd/i18n";
 
 describe('StopCommandComponent', () => {
   let component: StopCommandComponent;
@@ -92,6 +96,7 @@ describe('StopCommandComponent', () => {
       ],
       declarations: [StopCommandComponent],
       providers: [
+        { provide: NZ_I18N, useValue: ru_RU },
         { provide: CommandsService, useValue: spyCommands },
         { provide: TimezoneConverterService, useValue: timezoneConverterServiceSpy }
       ]
