12324602b47a06b083ea11b423965fc782162132
Daniil Markov <d.markov@intellect.group>
2022-08-16 16:50:29 +0300

    add price tooltip

--- src/app/modules/all-instruments/widgets/all-instruments-widget/all-instruments-widget.component.ts
+++ src/app/modules/all-instruments/widgets/all-instruments-widget/all-instruments-widget.component.ts
@@ -1,40 +1,30 @@
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
-import { DashboardItem } from "../../../../shared/models/dashboard-item.model";
-import { Subject, takeUntil } from "rxjs";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DashboardItem, DashboardItemContentSize } from "../../../../shared/models/dashboard-item.model";
+import { map, Observable, shareReplay } from "rxjs";
 
 @Component({
   selector: 'ats-all-instruments-widget',
   templateUrl: './all-instruments-widget.component.html',
   styleUrls: ['./all-instruments-widget.component.less']
 })
-export class AllInstrumentsWidgetComponent implements OnInit, OnDestroy, OnChanges {
+export class AllInstrumentsWidgetComponent implements OnInit {
   @Input() public shouldShowSettings!: boolean;
   @Input() public guid!: string;
   @Input() public resize!: EventEmitter<DashboardItem>;
   @Output() public shouldShowSettingsChange = new EventEmitter<boolean>();
-
-  private destroy$: Subject<boolean> = new Subject<boolean>();
-  private resizeLastValue!: DashboardItem;
+  contentSize$!: Observable<DashboardItemContentSize>;
 
   ngOnInit() {
-    this.resize.pipe(
-      takeUntil(this.destroy$)
-    )
-      .subscribe(val => this.resizeLastValue = val);
+    this.contentSize$ = this.resize.pipe(
+      map(x => ({
+        height: x.height,
+        width: x.width
+      } as DashboardItemContentSize)),
+      shareReplay(1)
+    );
   }
 
   onSettingsChange() {
     this.shouldShowSettingsChange.emit(!this.shouldShowSettings);
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.shouldShowSettings && !changes.shouldShowSettings.currentValue) {
-      setTimeout(() => this.resize.emit(this.resizeLastValue), 0);
-    }
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next(true);
-    this.destroy$.complete();
   }
 }
