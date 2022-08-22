0ccc8e7c5e0c17b8a0517f03286221af6bca144e
Agababaev Dmitrij <agababaev@alor.ru>
2022-08-22 19:05:23 +0300

    test

---
+++ .husky/common.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+command_exists () {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Windows 10, Git Bash and Yarn workaround
+if command_exists winpty && test -t 1; then
+  exec < /dev/tty
+fi
