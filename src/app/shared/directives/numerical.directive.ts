7e578b77fa35082ec99b8a7c4c6bfde923522fa1
Daniil Markov <d.markov@intellect.group>
2022-12-06 15:01:14 +0300

    add keyboard arrows value changing

--- src/app/shared/directives/numerical.directive.ts
+++ src/app/shared/directives/numerical.directive.ts
@@ -1,13 +1,17 @@
 import {
   Directive,
   ElementRef,
-  HostListener
+  HostListener,
+  Input
 } from '@angular/core';
+import { MathHelper } from "../utils/math-helper";
 
 @Directive({
   selector: 'input[atsNumerical]'
 })
 export class NumericalDirective {
+
+  @Input() atsNumerical: string | number | boolean = 1;
 
   constructor(private _el: ElementRef) {
   }
@@ -31,7 +35,28 @@ export class NumericalDirective {
     }
   }
 
-  isInvalidValue(newSymbol: string): boolean {
+  @HostListener('keydown', ['$event']) onKeyDown(event: KeyboardEvent) {
+    let step = +(this.atsNumerical || 1);
+
+    if (event.code === 'ArrowDown') {
+      step = -step;
+    }
+
+    if (event.code === 'ArrowDown' || event.code === 'ArrowUp') {
+      event.stopPropagation();
+      event.preventDefault();
+      this._el.nativeElement.value = this.getStepSum(step);
+      this._el.nativeElement.dispatchEvent(new Event('input'));
+    }
+  }
+
+  private isInvalidValue(newSymbol: string): boolean {
     return !newSymbol || ['.', ','].includes(newSymbol) && (this._el.nativeElement.value.includes('.') || !this._el.nativeElement.value);
+  }
+
+  private getStepSum(step: number): number {
+    const inputValueNumber = +this._el.nativeElement.value || 0;
+    const roundingDecimals = Math.max(MathHelper.getPrecision(step), MathHelper.getPrecision(inputValueNumber));
+    return MathHelper.round(inputValueNumber + step, roundingDecimals);
   }
 }
