b7c32bcac6af0a4d9953c6b0c2d57d849a227d44
sbelashevskiy <sbelashevskiy@gmail.com>
2023-01-25 17:40:08 +0500

    Issue #752: Removed extra file

--- src/app/modules/light-chart/services/light-chart-default-settings-factory.ts
+++
@@ -1,7 +0,0 @@
-import { WidgetDefaultSettingsFactory } from '../../../shared/services/widget-settings/widget-settings-factory';
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class LightChartDefaultSettingsFactory implements WidgetDefaultSettingsFactory {
-
-}
