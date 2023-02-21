6233d9f5a13eb993d296657dbfff2f4d071d4787
sbelashevskiy <sbelashevskiy@gmail.com>
2023-02-21 10:15:18 +0500

    Added debug logging

--- src/app/modules/login/components/external-logout/external-logout.component.ts
+++ src/app/modules/login/components/external-logout/external-logout.component.ts
@@ -3,6 +3,7 @@ import {
   OnInit
 } from '@angular/core';
 import { LocalStorageService } from '../../../../shared/services/local-storage.service';
+import { LoggerService } from '../../../../shared/services/logger.service';
 
 @Component({
   selector: 'ats-external-logout',
@@ -10,12 +11,19 @@ import { LocalStorageService } from '../../../../shared/services/local-storage.s
   styleUrls: ['./external-logout.component.less']
 })
 export class ExternalLogoutComponent implements OnInit {
-  constructor(private readonly localStorageService: LocalStorageService) {
+  constructor(
+    private readonly localStorageService: LocalStorageService,
+    private readonly logger: LoggerService
+  ) {
   }
 
   ngOnInit() {
     const eventListener = (e: any) => {
       try {
+        this.logger.info(`[ExternalLogoutComponent]: Message received.`);
+        this.logger.info(`Data: ${JSON.stringify(e.data)}`);
+        this.logger.info(`Origin: ${JSON.stringify(e.origin)}`);
+
         const json = JSON.parse(e.data);
         const origins = [
           'localhost:8001',
@@ -27,6 +35,7 @@ export class ExternalLogoutComponent implements OnInit {
         const currentOrigin = new URL(e.origin);
         if (origins.includes(currentOrigin.host) && json?.source === 'sso') {
           this.localStorageService.removeItem('user');
+          this.logger.info('Logout');
 
           window.removeEventListener('message', eventListener);
         }
