a637f228ad21ce4098560498667b85f932da8a6e
sbelashevskiy <sbelashevskiy@gmail.com>
2023-05-23 12:42:13 +0500

    Issue #945: Added notifications saving

--- src/app/modules/push-notifications/services/push-notifications-provider.ts
+++ src/app/modules/push-notifications/services/push-notifications-provider.ts
@@ -1,84 +1,134 @@
-﻿import {Injectable} from '@angular/core';
+﻿import {Injectable, OnDestroy} from '@angular/core';
 import {NotificationsProvider} from '../../notifications/services/notifications-provider';
-import { BehaviorSubject, Observable, switchMap, take } from 'rxjs';
+import {combineLatest, Observable, shareReplay, takeUntil} from 'rxjs';
 import {NotificationMeta} from '../../notifications/models/notification.model';
 import {PushNotificationsService} from "./push-notifications.service";
 import {Store} from "@ngrx/store";
-import {selectPortfoliosState} from "../../../store/portfolios/portfolios.selectors";
 import {filter, map} from "rxjs/operators";
-import {EntityStatus} from "../../../shared/models/enums/entity-status";
-import {PortfolioExtended} from "../../../shared/models/user/portfolio-extended.model";
-import { TerminalSettingsService } from "../../terminal-settings/services/terminal-settings.service";
-import { isPortfoliosEqual } from "../../../shared/utils/portfolios";
+import {TerminalSettingsService} from "../../terminal-settings/services/terminal-settings.service";
+import {isPortfoliosEqual} from "../../../shared/utils/portfolios";
+import {LocalStorageService} from "../../../shared/services/local-storage.service";
+import {Destroyable} from "../../../shared/utils/destroyable";
+import {PortfoliosStreams} from "../../../store/portfolios/portfolios.streams";
+import {TimezoneConverterService} from "../../../shared/services/timezone-converter.service";
+
+interface SavedPushNotification {
+  id: string;
+  timestamp: number;
+  title: string;
+  description: string;
+  isRead: boolean;
+}
 
 @Injectable()
-export class PushNotificationsProvider implements NotificationsProvider {
-  private notifications$?: BehaviorSubject<NotificationMeta[]>;
+export class PushNotificationsProvider implements NotificationsProvider, OnDestroy {
+  private readonly destroyable = new Destroyable();
+  private readonly pushNotificationsStorageKey = 'push-notifications';
 
-  private allMessages = new Map<string, NotificationMeta>();
+  private notifications$?: Observable<NotificationMeta[]>;
 
   constructor(
     private readonly pushNotificationsService: PushNotificationsService,
     private readonly store: Store,
-    private readonly terminalSettingsService: TerminalSettingsService
+    private readonly terminalSettingsService: TerminalSettingsService,
+    private readonly localStorageService: LocalStorageService,
+    private readonly timezoneConverterService: TimezoneConverterService
   ) {
   }
 
   getNotifications(): Observable<NotificationMeta[]> {
     if (!this.notifications$) {
-      this.initNotifications();
+      this.initRequiredSubscriptions();
+      this.initNotificationsSync();
+      this.notifications$ = this.getPushNotifications();
     }
 
-    return this.notifications$!.asObservable();
+    return this.notifications$;
   }
 
-  private initNotifications() {
-    this.notifications$ = new BehaviorSubject<NotificationMeta[]>([]);
+  ngOnDestroy(): void {
+    this.destroyable.destroy();
+  }
 
-    this.store.select(selectPortfoliosState).pipe(
-      filter(p => p.status === EntityStatus.Success),
-      map(portfoliosState => Object.values(portfoliosState.entities) as PortfolioExtended[]),
-      take(1),
-      map(allPortfolios => allPortfolios.map(p => ({portfolio: p.portfolio, exchange: p.exchange}))),
-      switchMap(allPortfolios => this.terminalSettingsService.getSettings()
-          .pipe(
-            map(s => s.instantNotificationsSettings?.hiddenPortfoliosForNotifications ?? []),
-            map(excludedPortfolios => allPortfolios.filter(p => !excludedPortfolios.find(ep => isPortfoliosEqual(p, ep)))),
-          ),
-      ),
-      switchMap(portfolios => this.pushNotificationsService.subscribeToOrdersExecute(portfolios)),
-      switchMap(() => this.pushNotificationsService.getMessages())
-    ).subscribe((payload) => {
-      if (!payload?.data?.body) {
-        return;
-      }
+  private getPushNotifications(): Observable<NotificationMeta[]> {
+    return combineLatest([
+      this.timezoneConverterService.getConverter(),
+      this.localStorageService.getItemStream<SavedPushNotification[]>(this.pushNotificationsStorageKey)
+    ]).pipe(
+      map(([converter, notifications]) => {
+        if (!notifications) {
+          return [];
+        }
 
-      const messageData = JSON.parse(payload.data.body).notification;
+        return notifications.map(n => ({
+            id: n.id,
+            title: n.title,
+            description: n.description,
+            date: converter.toTerminalDate(new Date(n.timestamp)),
+            isRead: n.isRead,
+            showDate: true,
+            markAsRead: () => this.setIsRead(n)
+          } as NotificationMeta)
+        );
+      }),
+      shareReplay(1)
+    );
+  }
 
+  private initRequiredSubscriptions() {
+    combineLatest([
+      PortfoliosStreams.getAllPortfolios(this.store),
+      this.terminalSettingsService.getSettings()
+    ]).pipe(
+      takeUntil(this.destroyable)
+    ).subscribe(([allPortfolios, terminalSettings]) => {
+      const portfolios = allPortfolios.map(p => ({portfolio: p.portfolio, exchange: p.exchange}));
+      const disableNotificationPortfolios = terminalSettings.instantNotificationsSettings?.hiddenPortfoliosForNotifications ?? [];
+
+      const filteredPortfolios = portfolios.filter(p => !disableNotificationPortfolios.find(ep => isPortfoliosEqual(p, ep)));
+
+      this.pushNotificationsService.subscribeToOrdersExecute(filteredPortfolios).subscribe();
+    });
+  }
+
+  private initNotificationsSync() {
+    this.pushNotificationsService.getMessages().pipe(
+      filter(payload => !!payload?.data?.body),
+      takeUntil(this.destroyable)
+    ).subscribe(payload => {
+      const messageData = JSON.parse(payload!.data!.body!).notification;
       if (!messageData) {
         return;
       }
 
-      const notification: NotificationMeta = {
-        id: payload.messageId,
-        date: new Date(),
-        title: messageData.title,
-        description: messageData.body,
-        isRead: false,
-        showDate: true,
-        markAsRead: () => {
-          this.allMessages.set(payload.messageId, {
-            ...notification,
-            isRead: true
-          });
+      const notifications = [
+        ...this.getSavedNotifications(),
+        {
+          id: payload.messageId,
+          title: messageData.title,
+          description: messageData.body,
+          timestamp: Date.now(),
+          isRead: false
+        } as SavedPushNotification
+      ];
 
-          this.notifications$!.next(Array.from(this.allMessages.values()));
-        }
-      };
+      const notificationsToSave = notifications
+        .sort((a, b) => a.timestamp - b.timestamp).slice(-50);
 
-      this.allMessages.set(payload.messageId, notification);
-
-      this.notifications$!.next(Array.from(this.allMessages.values()));
+      this.localStorageService.setItem(this.pushNotificationsStorageKey, notificationsToSave);
     });
+  }
+
+  private getSavedNotifications(): SavedPushNotification[] {
+    return this.localStorageService.getItem<SavedPushNotification[]>(this.pushNotificationsStorageKey) ?? [];
+  }
+
+  private setIsRead(notification: SavedPushNotification) {
+    const currentNotifications = this.getSavedNotifications();
+    const updated = [
+      ...currentNotifications.filter(n => n.id !== notification.id),
+    ];
+
+    this.localStorageService.setItem(this.pushNotificationsStorageKey, updated);
   }
 }
