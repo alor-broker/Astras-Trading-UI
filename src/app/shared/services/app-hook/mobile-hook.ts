a588cc4aac4c9753a627788a53b7d753d2e6f1fb
sbelashevskiy <sbelashevskiy@gmail.com>
2023-05-29 14:18:36 +0500

    Issue #946: Try to fix extra scrolling

---
+++ src/app/shared/services/app-hook/mobile-hook.ts
@@ -0,0 +1,26 @@
+﻿import {Inject, Injectable} from "@angular/core";
+import {AppHook} from "./app-hook-token";
+import {take} from "rxjs";
+import {DeviceService} from "../device.service";
+import {filter} from "rxjs/operators";
+import {DOCUMENT} from "@angular/common";
+
+@Injectable()
+export class MobileHook implements AppHook {
+  constructor(
+    private readonly deviceService: DeviceService,
+    @Inject(DOCUMENT) private readonly document: Document) {
+  }
+
+  onDestroy(): void {
+  }
+
+  onInit(): void {
+    this.deviceService.deviceInfo$.pipe(
+      take(1),
+      filter(info => info.isMobile)
+    ).subscribe(() => {
+      this.document.documentElement.classList.add('mobile');
+    });
+  }
+}
