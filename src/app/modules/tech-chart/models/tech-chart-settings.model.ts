ec83a4b4a5381ccb2c69218277fca0896c2e08a6
sbelashevskiy <sbelashevskiy@gmail.com>
2023-06-30 17:38:16 +0500

    Issue #1045: Save chart layout

--- src/app/modules/tech-chart/models/tech-chart-settings.model.ts
+++ src/app/modules/tech-chart/models/tech-chart-settings.model.ts
@@ -2,5 +2,5 @@ import { WidgetSettings } from '../../../shared/models/widget-settings.model';
 import { InstrumentKey } from '../../../shared/models/instruments/instrument-key.model';
 
 export interface TechChartSettings extends WidgetSettings, InstrumentKey {
-  chartSettings: { [key: string]: string };
+  chartLayout?: object;
 }
