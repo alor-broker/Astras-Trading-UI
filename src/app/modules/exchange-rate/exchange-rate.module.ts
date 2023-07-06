7c048d77db760f05d0f149af5e0dcebe6858eeef
sbelashevskiy <sbelashevskiy@gmail.com>
2023-07-06 09:03:23 +0500

    Issue #1040: Added scroll

--- src/app/modules/exchange-rate/exchange-rate.module.ts
+++ src/app/modules/exchange-rate/exchange-rate.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ExchangeRateComponent } from './components/exchange-rate/exchange-rate.component';
 import { ExchangeRateWidgetComponent } from './widgets/exchange-rate-widget/exchange-rate-widget.component';
 import { SharedModule } from "../../shared/shared.module";
+import {NzResizeObserverModule} from "ng-zorro-antd/cdk/resize-observer";
 
 
 @NgModule({
@@ -13,10 +14,11 @@ import { SharedModule } from "../../shared/shared.module";
   exports: [
     ExchangeRateWidgetComponent
   ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ]
+    imports: [
+        CommonModule,
+        SharedModule,
+        NzResizeObserverModule
+    ]
 })
 export class ExchangeRateModule {
 }
