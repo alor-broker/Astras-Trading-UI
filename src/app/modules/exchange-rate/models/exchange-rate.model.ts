7f649cbf074bd63a1802ae6322dd8d1fee929e92
sbelashevskiy <sbelashevskiy@gmail.com>
2023-12-20 16:06:44 +0500

    Issue #1354: Added cross rates

---
+++ src/app/modules/exchange-rate/models/exchange-rate.model.ts
@@ -0,0 +1,11 @@
+﻿export interface RateValue {
+  rate: number;
+  sourceSymbol: string | null;
+}
+
+export interface Rate {
+  fromCurrency: string;
+  toCurrency: string;
+  symbolTom: string;
+  lastPrice: number;
+}
