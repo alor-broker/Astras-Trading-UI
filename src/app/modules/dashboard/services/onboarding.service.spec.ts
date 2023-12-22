55d13af056a4c12df6d7c1efb2bd46a5a73ba53b
Daniil Markov <45139991+agushasok@users.noreply.github.com>
2023-12-22 11:41:13 +0300

    #1347 fix tutorial width and buttons (#1431)
    * #1347 fix tutorial width and buttons
    
    * #1347 PR fixes
    
    ---------
    
    Co-authored-by: Daniil Markov <d.markov@intellect.group>

--- src/app/modules/dashboard/services/onboarding.service.spec.ts
+++ src/app/modules/dashboard/services/onboarding.service.spec.ts
@@ -2,11 +2,13 @@ import {TestBed} from '@angular/core/testing';
 import {JoyrideService} from 'ngx-joyride';
 import {OnboardingService} from './onboarding.service';
 import {LocalStorageService} from "../../../shared/services/local-storage.service";
+import { TranslatorService } from "../../../shared/services/translator.service";
 
 describe('OnboardingService', () => {
   let service: OnboardingService;
   let localStorageServiceSpy: any;
   let joyrideServiceSpy: any;
+  let translatorServiceSpy: any;
 
 
   beforeAll(() => TestBed.resetTestingModule());
@@ -14,6 +16,7 @@ describe('OnboardingService', () => {
   beforeEach(() => {
     joyrideServiceSpy = jasmine.createSpyObj('JoyrideService', ['startTour']);
     localStorageServiceSpy = jasmine.createSpyObj('LocalStorageService', ['getItem', 'setItem']);
+    translatorServiceSpy = jasmine.createSpyObj('TranslatorServiceSpy', ['getTranslator']);
   });
 
   beforeEach(() => {
@@ -21,7 +24,8 @@ describe('OnboardingService', () => {
       providers: [
         OnboardingService,
         {provide: JoyrideService, useValue: joyrideServiceSpy},
-        {provide: LocalStorageService, useValue: localStorageServiceSpy}
+        {provide: LocalStorageService, useValue: localStorageServiceSpy},
+        {provide: TranslatorService, useValue: translatorServiceSpy}
       ]
     });
 
