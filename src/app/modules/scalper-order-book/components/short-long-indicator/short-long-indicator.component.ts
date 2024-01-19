8e853c35f3219be7e2ab6b6b51dbcc75fd1973e7
sbelashevskiy <sbelashevskiy@gmail.com>
2024-01-19 17:39:24 +0500

    Issue #1402: Remove extra code

--- src/app/modules/scalper-order-book/components/short-long-indicator/short-long-indicator.component.ts
+++ src/app/modules/scalper-order-book/components/short-long-indicator/short-long-indicator.component.ts
@@ -15,7 +15,6 @@ import {
   shareReplay,
   switchMap,
   take,
-  tap,
   timer
 } from "rxjs";
 import {
@@ -68,8 +67,7 @@ export class ShortLongIndicatorComponent implements OnInit {
       currentOrders$
     ]).pipe(
       switchMap(() => updateInterval$),
-      switchMap(interval => timer(0, interval * 1000)),
-      tap(() => console.log('trigger'))
+      switchMap(interval => timer(0, interval * 1000))
     );
 
     const dataStream$ = defer(() => {
@@ -93,7 +91,6 @@ export class ShortLongIndicatorComponent implements OnInit {
     this.shortLongValues$ = refreshTrigger$.pipe(
       switchMap(() => dataStream$),
       switchMap(d => {
-        console.log(d);
         const bestAsk: number | null = d.orderBookData.a[0]?.p ?? d.orderBookData.b[0]?.p ?? null;
         const bestBid: number | null = d.orderBookData.b[0]?.p ?? d.orderBookData.a[0]?.p ?? null;
 
