c8d293e40ade9cdf1f7720ff09ae4ac194060e7c
sbelashevskiy <sbelashevskiy@gmail.com>
2024-11-15 11:11:16 +0500

    Issue #1828: Restore dev config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,14 +12,14 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://api.alor.ru',
-  wsUrl: 'wss://api.alor.ru/ws',
-  cwsUrl: 'wss://api.alor.ru/cws',
-  clientDataUrl: 'https://lk-api.alor.ru',
-  ssoUrl: 'https://login.alor.ru',
+  apiUrl: 'https://apidev.alor.ru',
+  wsUrl: 'wss://apidev.alor.ru/ws',
+  cwsUrl: 'wss://apidev.alor.ru/cws',
+  clientDataUrl: 'https://lk-api-dev.alorbroker.ru',
+  ssoUrl: 'https://login-dev.alor.ru',
   warpUrl: 'https://warp.alor.dev',
-  remoteSettingsStorageUrl: 'https://astras.alor.ru/identity/v5/UserSettings',
-  teamlyDatabaseUrl: 'https://astras.alor.ru/teamly/api/v1/ql/content-database/content',
+  remoteSettingsStorageUrl: 'https://astras-dev.alor.ru/identity/v5/UserSettings',
+  teamlyDatabaseUrl: 'https://astras-dev.alor.ru/teamly/api/v1/ql/content-database/content',
   alorIconsStorageUrl: 'https://storage.alorbroker.ru/icon/',
   logging: {
     console: {
