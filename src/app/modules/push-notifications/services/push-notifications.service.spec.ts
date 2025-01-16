fefadb15fb286a5001bbac8df454a894bd077829
Sergey Belashevsky <sbelashevskiy@gmail.com>
2025-01-16 17:01:49 +0500

    FCM libs update (#1898)
    * Issue #1884: Update FCM libs
    
    * Issue #1884: Fixed build
    
    * Issue #1884: Fixed permissions request
    
    * Issue #1884: Fixed notifications duplication

--- src/app/modules/push-notifications/services/push-notifications.service.spec.ts
+++ src/app/modules/push-notifications/services/push-notifications.service.spec.ts
@@ -1,65 +1,42 @@
-import {TestBed} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-import {PushNotificationsService} from './push-notifications.service';
+import { PushNotificationsService } from './push-notifications.service';
 import { provideHttpClientTesting } from "@angular/common/http/testing";
-import {AngularFireMessaging} from "@angular/fire/compat/messaging";
-import {of, Subject} from "rxjs";
-import {ErrorHandlerService} from "../../../shared/services/handle-error/error-handler.service";
-import {TimezoneConverterService} from "../../../shared/services/timezone-converter.service";
-import {TimezoneConverter} from "../../../shared/utils/timezone-converter";
-import {TimezoneDisplayOption} from "../../../shared/models/enums/timezone-display-option";
-import {LocalStorageService} from "../../../shared/services/local-storage.service";
+import { ErrorHandlerService } from "../../../shared/services/handle-error/error-handler.service";
 import { EnvironmentService } from "../../../shared/services/environment.service";
 import { TranslatorService } from "../../../shared/services/translator.service";
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi
+} from '@angular/common/http';
+import { MockProvider } from "ng-mocks";
+import { LoggerService } from "../../../shared/services/logging/logger.service";
 
 describe('FirebaseNotificationsService', () => {
   let service: PushNotificationsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [],
-    providers: [
-        {
-            provide: AngularFireMessaging,
-            useValue: {
-                requestToken: of('testToken')
-            }
-        },
-        {
-            provide: ErrorHandlerService,
-            useValue: {}
-        },
-        {
-            provide: LocalStorageService,
-            useValue: {
-                getItemStream: jasmine.createSpy('getItemStream').and.returnValue(new Subject()),
-                setItem: jasmine.createSpy('setItem').and.callThrough(),
-                getItem: jasmine.createSpy('getItem').and.returnValue(undefined),
-            }
-        },
-        {
-            provide: TimezoneConverterService,
-            useValue: {
-                getConverter: jasmine.createSpy('getConverter').and.returnValue(of(new TimezoneConverter(TimezoneDisplayOption.MskTime)))
-            }
-        },
-        {
-            provide: EnvironmentService,
-            useValue: {
-                apiUrl: ''
-            }
-        },
-        {
-            provide: TranslatorService,
-            useValue: {
-                getActiveLang: jasmine.createSpy('getActiveLang').and.returnValue('ru')
-            }
-        },
+      imports: [],
+      providers: [
+        MockProvider(
+          EnvironmentService,
+          {
+            apiUrl: ''
+          }
+        ),
+        MockProvider(ErrorHandlerService),
+        MockProvider(
+          TranslatorService,
+          {
+            getActiveLang: jasmine.createSpy('getActiveLang').and.returnValue('ru')
+          }
+        ),
+        MockProvider(LoggerService),
         provideHttpClient(withInterceptorsFromDi()),
         provideHttpClientTesting()
-    ]
-});
+      ]
+    });
     service = TestBed.inject(PushNotificationsService);
   });
 
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,7 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.19.1/firebase-app-compat.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.19.1/firebase-messaging-compat.js",
-);
+importScripts('https://www.gstatic.com/firebasejs/11.1.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.1.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyCI0yjnNuT8VWJG4ow38-iY231ZoTGxV-o",
@@ -15,6 +13,11 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
+  if(payload.notification != null) {
+    // messages with notification are displayed by default
+    return;
+  }
+
   if (!payload.data.body) {
     return;
   }
@@ -22,8 +25,12 @@ messaging.onBackgroundMessage((payload) => {
   const messageData = JSON.parse(payload.data.body).notification;
 
   if (messageData) {
-    return self.registration.showNotification(messageData.title,
-      messageData
+    return self.registration.showNotification(
+      messageData.title,
+      {
+        ...messageData,
+        icon: '/assets/custom_icons/outline/ats-logo.svg'
+      }
     );
   }
 });
