f34cc58c75296b955af13d13c914545a84c24854
sbelashevskiy <sbelashevskiy@gmail.com>
2025-01-28 14:51:10 +0500

    Issue #1902: Added migration to enable new widget

--- src/app/modules/settings-migration/dashboard-settings/dashboard-settings-mobile-migration-manager.ts
+++ src/app/modules/settings-migration/dashboard-settings/dashboard-settings-mobile-migration-manager.ts
@@ -1,14 +1,14 @@
-﻿import { MobileMigrationManagerBase } from "../mobile-migration-manager-base";
-import { LocalStorageService } from "../../../shared/services/local-storage.service";
-import { Injectable } from "@angular/core";
+﻿import {MobileMigrationManagerBase} from "../mobile-migration-manager-base";
+import {LocalStorageService} from "../../../shared/services/local-storage.service";
+import {inject, Injectable} from "@angular/core";
+import {AddHomeScreenMobileDashboardSettings} from "./migrations/add-home-screen-mobile-dashboard.migration";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardSettingsMobileMigrationManager extends MobileMigrationManagerBase {
   protected migrations = [
-    // UpdateBadgesMobileDashboardMigration is obsolete. Keep here just as example
-    // inject(UpdateBadgesMobileDashboardMigration)
+    inject(AddHomeScreenMobileDashboardSettings)
   ];
 
   constructor(protected readonly localStorageService: LocalStorageService) {
