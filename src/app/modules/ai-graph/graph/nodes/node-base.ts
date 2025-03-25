18ad58011973c6d57994446a373b64fd8a87d718
Konstantin <bormotovk@gmail.com>
2025-03-25 16:37:49 +0100

    fix: Allow setOutputByName to accept PortfolioKey type

--- src/app/modules/ai-graph/graph/nodes/node-base.ts
+++ src/app/modules/ai-graph/graph/nodes/node-base.ts
@@ -68,7 +68,7 @@ export class NodeBase extends LGraphNode {
     );
   }
 
-  setOutputByName(outputName: string, data: number | string | boolean | OutputDataObject): void {
+  setOutputByName(outputName: string, data: number | string | boolean | OutputDataObject | PortfolioKey): void {
     const outputIndex = this.findOutputSlot(outputName);
     if (outputIndex >= 0) {
       this.setOutputData(
